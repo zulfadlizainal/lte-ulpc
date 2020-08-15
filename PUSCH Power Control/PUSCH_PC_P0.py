a10e6d3d9a760659b18fa77269609704d34fbdd6
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2020-08-15 11:39:35 +0900

    v1.1_PUSCH_P0Based
    Complete codes

--- PUSCH Power Control/PUSCH_PC_P0.py
+++ PUSCH Power Control/PUSCH_PC_P0.py
@@ -5,11 +5,8 @@
 import pandas as pd
 import numpy as np
 import math
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 # Input Max UE TX Power Capability
 
@@ -87,43 +84,99 @@ df_prb_plot = df_prb
 df_prb_plot = df_prb_plot.drop(['Path Loss (dB)'], axis=1)
 df_prb_plot = df_prb_plot.set_index('RSRP (dBm)')
 
-df_prb_plot.plot()
+# Plot
 
-# x = np.arange(len(df_prb_plot.columns))
-# y = np.arange(len(df_prb_plot.index))
-# X, Y = np.meshgrid(x, y)
-# Z = df_prb_plot
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet,
-#                        linewidth=0, antialiased=False)
+xmin = 0
+xmax = max_ul_prb
+ymin = df_prb.iloc[-1]['RSRP (dBm)']
+ymax = df_prb.iloc[0]['RSRP (dBm)']
 
-# fig.colorbar(surf, shrink=0.5, aspect=5)
-# plt.title('Original Code')
+plt.imshow(df_prb_plot, cmap=cm.jet, interpolation='hermite',
+           extent=[xmin, xmax, ymin, ymax])
 
-# ax = plt.gca()
-# ax.set_xticks(np.arange(0, max_ul_prb, 10))
-# ax.set_yticks(np.arange(-60, -150, 10))
+plt.xlabel("UL Scheduled PRB")
+plt.ylabel("RSRP (dBm)")
+
+plt.title(
+    f'UL PUSCH TX Power (dBm)\nAssumptions: p0 = {p0_param}dBm, alpha = {alpha_param}\n')
+plt.grid(which='major', axis='both', linestyle='--')
+
+plt.colorbar()
 
 plt.show()
 
+########### Study 2: How much Tx Power is affected by alpha ###########
 
-# ######Plot RSRP Map######
+prb_param = 100  # Assume
+p0_param = -92  # Assume
 
-# plt.imshow(df_prb_plot, cmap='RdYlGn', interpolation='hermite')
+df_alpha = pd.DataFrame({'Path Loss (dB)': pl})
+df_alpha['RSRP (dBm)'] = (df_alpha['Path Loss (dB)'] - rs_power) * -1
 
-# #plt.colorbar()
+index = 0
 
-# plt.xlabel("Distance (m)")
-# plt.ylabel("Distance (m)")
+for index in range(len(alpha)):
 
-# ax = plt.gca()
-# ax.set_xticks(np.arange(grid*(-1), grid, (step*20)))
-# ax.set_yticks(np.arange(grid*(-1), grid, (step*20)))
+    df_alpha[f'{alpha[index]}'] = (
+        10*math.log10(prb_param)) + p0_param + (alpha[index]*df_alpha['Path Loss (dB)'])
 
-# plt.title("UL PUSCH TX Power (dBm)")
-# plt.grid(which='major', axis='both', linestyle='--')
+df_alpha[df_alpha > max_tx_power] = max_tx_power
 
-# plt.colorbar()
+df_alpha_plot = df_alpha
+df_alpha_plot = df_alpha_plot.drop(['Path Loss (dB)'], axis=1)
+df_alpha_plot = df_alpha_plot.set_index('RSRP (dBm)')
+df_alpha_plot_filter = df_alpha_plot[df_alpha_plot.columns[-4:]]
 
-# plt.show()
+# Plot
+
+df_alpha_plot_filter.plot(style='--')
+
+plt.xlabel("RSRP (dBm)")
+plt.ylabel("UE TX Power (dBm)")
+plt.title(f'UL PUSCH TX Power (dBm)\nAssumptions: UL Scheduled PRB = {prb_param}, p0 = {p0_param}dBm\n')
+plt.legend(title='alpha')
+plt.grid()
+
+plt.show()
+
+########### Study 3: How much Tx Power is affected by P0 ###########
+
+alpha_param = 1  # Assume
+prb_param = 100  # Assume
+
+df_p0 = pd.DataFrame({'Path Loss (dB)': pl})
+df_p0['RSRP (dBm)'] = (df_p0['Path Loss (dB)'] - rs_power) * -1
+
+index = 0
+
+for index in range(len(p0)):
+
+    df_p0[f'{p0[index]}'] = (
+        10*math.log10(prb_param)) + p0[index] + (alpha_param*df_p0['Path Loss (dB)'])
+
+df_p0[df_p0 > max_tx_power] = max_tx_power
+
+df_p0_plot = df_p0
+df_p0_plot = df_p0_plot.drop(['Path Loss (dB)'], axis=1)
+df_p0_plot = df_p0_plot.set_index('RSRP (dBm)')
+
+# Plot
+
+xmin = p0[0]
+xmax = p0[-1]
+ymin = df_p0.iloc[-1]['RSRP (dBm)']
+ymax = df_p0.iloc[0]['RSRP (dBm)']
+
+plt.imshow(df_p0_plot, cmap=cm.jet, interpolation='hermite',
+           extent=[xmin, xmax, ymin, ymax])
+
+plt.xlabel("p0 (dBm)")
+plt.ylabel("RSRP (dBm)")
+
+plt.title(
+    f'UL PUSCH TX Power (dBm)\nAssumptions: UL Scheduled PRB = {prb_param}, alpha = {alpha_param}\n')
+plt.grid(which='major', axis='both', linestyle='--')
+
+plt.colorbar()
+
+plt.show()
