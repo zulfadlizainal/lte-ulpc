234f161fad4bee4d1fe4fe9aebd6ec6a90a7e035
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2020-08-23 12:14:20 +0900

    v1.2_PUSCH_P0Based
    Update Codes

--- PUSCH Power Control/PUSCH_PC_SINR.py
+++ PUSCH Power Control/PUSCH_PC_SINR.py
@@ -1,12 +1,12 @@
-# Author: Zulfadli Zainal
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import pandas as pd
+23  # Author: Zulfadli Zainal
 # Github: https://github.com/zulfadlizainal
 # Linkedin: https://linkedin.com/in/zulfadlizainal
 
-import pandas as pd
-import numpy as np
-import math
-import matplotlib.pyplot as plt
-from matplotlib import cm
 
 # Input Max UE TX Power Capability
 
@@ -86,7 +86,8 @@ df_prb_plot[df_prb_plot > max_tx_power] = max_tx_power
 
 # Calculate UL SINR
 
-ul_rssi = -115  # Assume
+ul_rssi = -100  # Assume
+target_ul_sinr = 23  # Assume
 
 df_pl = pd.DataFrame({'Path Loss (dB)': pl})
 df_pl['RSRP (dBm)'] = (df_pl - rs_power) * -1
@@ -98,99 +99,33 @@ df_prb_plot_ulrx = df_prb_plot_ulrx.sub(df_pl['Path Loss (dB)'], axis=0)
 df_prb_plot_sinr = (pow(10, (df_prb_plot_ulrx)/10)) / (pow(10, (ul_rssi)/10))
 df_prb_plot_sinr = 10*np.log10(df_prb_plot_sinr)
 
-# # Plot
+# Calculate How much Power Need to Compensate to Meet SINR Target
 
-# xmin = 0
-# xmax = max_ul_prb
-# ymin = df_prb.iloc[-1]['RSRP (dBm)']
-# ymax = df_prb.iloc[0]['RSRP (dBm)']
+df_prb_plot_sinr_delta = target_ul_sinr - df_prb_plot_sinr
+df_prb_plot_sinr_delta[df_prb_plot_sinr_delta <= 0] = 0
 
-# plt.imshow(df_prb_plot, cmap=cm.jet, interpolation='hermite',
-#            extent=[xmin, xmax, ymin, ymax])
-
-# plt.xlabel("UL Scheduled PRB")
-# plt.ylabel("RSRP (dBm)")
-
-# plt.title(
-#     f'UL PUSCH TX Power (dBm)\nAssumptions: p0 = {p0_param}dBm, alpha = {alpha_param}\n')
-# plt.grid(which='major', axis='both', linestyle='--')
-
-# plt.colorbar()
-
-# plt.show()
-
-# ########### Study 2: How much Tx Power is affected by alpha ###########
-
-# prb_param = 100  # Assume
-# p0_param = -92  # Assume
-
-# df_alpha = pd.DataFrame({'Path Loss (dB)': pl})
-# df_alpha['RSRP (dBm)'] = (df_alpha['Path Loss (dB)'] - rs_power) * -1
-
-# index = 0
-
-# for index in range(len(alpha)):
-
-#     df_alpha[f'{alpha[index]}'] = (
-#         10*math.log10(prb_param)) + p0_param + (alpha[index]*df_alpha['Path Loss (dB)'])
-
-# df_alpha[df_alpha > max_tx_power] = max_tx_power
-
-# df_alpha_plot = df_alpha
-# df_alpha_plot = df_alpha_plot.drop(['Path Loss (dB)'], axis=1)
-# df_alpha_plot = df_alpha_plot.set_index('RSRP (dBm)')
-# df_alpha_plot_filter = df_alpha_plot[df_alpha_plot.columns[-4:]]
+df_prb_plot = df_prb_plot + df_prb_plot_sinr_delta
+df_prb_plot[df_prb_plot > max_tx_power] = max_tx_power
 
 # # Plot
 
-# df_alpha_plot_filter.plot(style='--')
+xmin = 0
+xmax = max_ul_prb
+ymin = df_prb.iloc[-1]['RSRP (dBm)']
+ymax = df_prb.iloc[0]['RSRP (dBm)']
+vmin = -20
+vmax = 23
 
-# plt.xlabel("RSRP (dBm)")
-# plt.ylabel("UE TX Power (dBm)")
-# plt.title(f'UL PUSCH TX Power (dBm)\nAssumptions: UL Scheduled PRB = {prb_param}, p0 = {p0_param}dBm\n')
-# plt.legend(title='alpha')
-# plt.grid()
+plt.imshow(df_prb_plot, cmap=cm.jet, interpolation='hermite',
+           vmin=vmin, vmax=vmax, extent=[xmin, xmax, ymin, ymax])
 
-# plt.show()
+plt.xlabel("UL Scheduled PRB")
+plt.ylabel("RSRP (dBm)")
 
-# ########### Study 3: How much Tx Power is affected by P0 ###########
+plt.title(
+    f'UL PUSCH TX Power (dBm)\nAssumptions: p0 = {p0_param}dBm, alpha = {alpha_param},\nUL RSSI = {ul_rssi}dBm, UL SINR Target = {target_ul_sinr}dB\n')
+plt.grid(which='major', axis='both', linestyle='--')
 
-# alpha_param = 1  # Assume
-# prb_param = 100  # Assume
+plt.colorbar()
 
-# df_p0 = pd.DataFrame({'Path Loss (dB)': pl})
-# df_p0['RSRP (dBm)'] = (df_p0['Path Loss (dB)'] - rs_power) * -1
-
-# index = 0
-
-# for index in range(len(p0)):
-
-#     df_p0[f'{p0[index]}'] = (
-#         10*math.log10(prb_param)) + p0[index] + (alpha_param*df_p0['Path Loss (dB)'])
-
-# df_p0[df_p0 > max_tx_power] = max_tx_power
-
-# df_p0_plot = df_p0
-# df_p0_plot = df_p0_plot.drop(['Path Loss (dB)'], axis=1)
-# df_p0_plot = df_p0_plot.set_index('RSRP (dBm)')
-
-# # Plot
-
-# xmin = p0[0]
-# xmax = p0[-1]
-# ymin = df_p0.iloc[-1]['RSRP (dBm)']
-# ymax = df_p0.iloc[0]['RSRP (dBm)']
-
-# plt.imshow(df_p0_plot, cmap=cm.jet, interpolation='hermite',
-#            extent=[xmin, xmax, ymin, ymax])
-
-# plt.xlabel("p0 (dBm)")
-# plt.ylabel("RSRP (dBm)")
-
-# plt.title(
-#     f'UL PUSCH TX Power (dBm)\nAssumptions: UL Scheduled PRB = {prb_param}, alpha = {alpha_param}\n')
-# plt.grid(which='major', axis='both', linestyle='--')
-
-# plt.colorbar()
-
-# plt.show()
+plt.show()
